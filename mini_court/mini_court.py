6d71a82e2d8546c30e948cf4c3e98eecbfdb741f
Aakarshan Verma <2022a1r170@mietjammu.in>
2025-10-31 19:36:17 +0530

    Updated

--- mini_court/mini_court.py
+++ mini_court/mini_court.py
@@ -26,7 +26,6 @@ class MiniCourt():
         self.set_court_drawing_key_points()
         self.set_court_lines()
 
-
     def convert_meters_to_pixels(self, meters):
         return convert_meters_to_pixel_distance(meters,
                                                 constants.DOUBLE_LINE_WIDTH,
